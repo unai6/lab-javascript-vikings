dc3321665793bdeabe334cf3762634ed224e0962
Unai Gonzalez <unaigoe91@gmail.com>
2020-03-15 12:46:25 +0100

    War fixed

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -68,8 +68,87 @@ receiveDamage(damage) {
 }
 
 const aSoldier = new Soldier()
-const aViking = new Viking ()
+const aViking = new Viking 
 const aSaxon = new Saxon()
 
 // W
-class War { }
+class War { 
+
+    constructor(){
+    this.vikingArmy = [],
+    this.saxonArmy = []
+
+    }
+
+    addViking(viking){
+    
+    this.vikingArmy.push(viking)
+    console.log('one Viking added')
+
+     
+    };
+
+
+    addSaxon(saxon){
+      this.saxonArmy.push(saxon)
+      console.log('one Saxon added')
+    }
+
+
+    vikingAttack() {
+    
+    let oneSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+
+    let oneViking = Math.floor(Math.random()* this.vikingArmy.length);
+
+    let randomSaxon = this.saxonArmy[oneSaxon];
+    let randomViking = this.vikingArmy[oneViking];
+
+    let remainingHealth = randomSaxon.receiveDamage(randomViking.strength);
+
+    if(randomSaxon.health = 0){
+      this.saxonArmy.splice(randomSaxon, 1)
+    
+    }
+     return remainingHealth;
+
+    };
+
+
+
+    saxonAttack(){
+    let oneSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+
+    let oneViking = Math.floor(Math.random()* this.vikingArmy.length);
+
+    let randomSaxon = this.saxonArmy[oneSaxon];
+    let randomViking = this.vikingArmy[oneViking];
+
+    
+    let remainingHealth = randomViking.receiveDamage(randomSaxon.strength);
+
+    if(randomViking.health = 0){
+      this.vikingArmy.splice(randomViking, 1)
+    
+    }
+     return remainingHealth
+
+    };
+
+
+    showStatus(){
+
+      if(saxonArmy.length === 0){
+        'Vikings have won the war of the century'
+      
+      } else if(vikingArmy.length === 0){
+        'Saxons have fought for their lives and survived another day...'
+      } else if(vikingArmy.length && saxonArmy.length === 1) {
+        'Vikings and Saxons are still in the thick of the battle'
+
+      } 
+       return showStatus
+
+    }
+
+};
